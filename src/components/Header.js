28252830230cd1fc27d4ce1eac211943b32ff94f
Ramsey <ramvandermeer@gmail.com>
2021-06-14 17:38:25 -0700

    Nav Working

--- src/components/Header.js
+++ src/components/Header.js
@@ -5,25 +5,22 @@ import Logo from '../img/Logo.svg'
 const Header = () => {
 
     const [expanded, setExpanded] = React.useState(false)
-    const [toggle, setToggle] = useState(false);
 
     const toggleDropdown = () => {
         setExpanded(!expanded);
     }
 
-    const actToggle = () => {
-        setToggle(!toggle)
-    }
-
     return (
         <header>
-            <div className={`main-header ${expanded ? "expanded" : "collapsed"} `}>
-                <div class="main-header__branding">
-                    <img className="main-header__branding__logo" src={Logo} alt="" />
-                    <p className="main-header__branding__title">Ramsey van der Meer</p>
-                </div>
+            <div className={`main-header ${expanded ? "expanded" : "collapsed"}`}>
 
+                <div className="main-header__title">
+                    <img className="main-header__title__img" src={Logo} alt="" />
+                    <p>Ramsey van der Meer</p>
+                </div>
                 <nav className="main-header__nav">
+                    {/* This is kind of jank but it works to help the flex-container center the nav links */}
+
                     <ul className="main-header__nav__links">
                         <li className="main-header__nav__link">
                             <div href="/" className="main-header__button" href="/about" >About Me</div>
@@ -39,6 +36,11 @@ const Header = () => {
                         </li>
                     </ul>
                 </nav>
+                <div onClick={toggleDropdown} className="main-header__nav__menu-icon">
+                    <div className={expanded ? "bar1 animateBar" : "bar bar1"}></div>
+                    <div className={expanded ? "bar2 animateBar" : "bar bar2"}></div>
+                    <div className={expanded ? "bar3 animateBar" : " bar3"}></div>
+                </div>
             </div>
         </header>
     )
