893a9e64cf14afcf95e183eb1ec692263537fc3b
Ramsey <ramvandermeer@gmail.com>
2021-08-19 09:29:01 -0500

    Home page finished

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,9 +4,14 @@ import './Main.scss'
 const Main = () => {
     return (
         <div className="main">
-            <p>
-            </p>
-
+        <body className="main">
+            <ul className="main__list-container">
+                <li data-text="About"><a className="main__box" href="/about">About Me</a></li>
+                <li data-text="Projects"><a className="main__box" href="/projects">Projects</a></li>
+                <li data-text="Photography"><a className="main__box" href="/photography">Photography</a></li>
+                <li data-text="Contact"><a className="main__box" href="/contact">contact</a></li>
+            </ul>
+        </body>
         </div>
     )
 }
